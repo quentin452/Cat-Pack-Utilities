7cf72b1f03feeefc44226e5a8a44288e7c8737bd
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-12-28 23:15:25 +0100

    Rewrite using tkinter instead of OpenGL

--- Cat-pack-utilities/CatPackUtilitiesWindow.py
+++ Cat-pack-utilities/CatPackUtilitiesWindow.py
@@ -1,122 +1,69 @@
-import glfw
-from OpenGL.GL import *
-import ctypes
-import logging
+import tkinter as tk
+from PIL import Image, ImageTk
 
-from OpenGL.GL import shaders
-from PIL import Image
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
+# Taille originale de l'image
+original_image_size = None
 
 
-def window_resize(window, width, height):
-    glViewport(0, 0, width, height)
+def resize_background(event):
+    global background_photo, background_image, background_canvas, original_image_size
+    # Obtention de la taille de la fenêtre
+    new_width = event.width
+    new_height = event.height
 
+    # Calcul du scale pour redimensionner l'image de façon proportionnelle
+    width_scale = new_width / original_image_size[0]
+    height_scale = new_height / original_image_size[1]
 
-def load_texture(file_path):
-    image = Image.open(file_path).transpose(Image.FLIP_TOP_BOTTOM)
-    img_data = image.tobytes("raw", "RGBA", 0, -1)
+    # Choix du scale le plus petit pour garder les bordures noires si nécessaire
+    scale = min(width_scale, height_scale)
 
-    texture = glGenTextures(1)
-    glBindTexture(GL_TEXTURE_2D, texture)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
-    glBindTexture(GL_TEXTURE_2D, 0)
+    # Redimensionnement de l'image avec le scale calculé
+    resized_width = int(original_image_size[0] * scale)
+    resized_height = int(original_image_size[1] * scale)
 
-    return texture
+    resized_image = background_image.resize((resized_width, resized_height), Image.LANCZOS)
+
+    # Création d'une image de fond noire de la taille de la fenêtre
+    black_background = Image.new('RGB', (new_width, new_height), color='black')
+    black_background.paste(resized_image, ((new_width - resized_width) // 2, (new_height - resized_height) // 2))
+
+    # Création de la nouvelle ImageTk.PhotoImage pour la fenêtre redimensionnée
+    background_photo = ImageTk.PhotoImage(black_background)
+
+    # Mise à jour de l'image affichée dans le canvas
+    canvas.itemconfig(background_canvas, image=background_photo)
 
 
 def main():
-    # Initialize GLFW
-    if not glfw.init():
-        logging.error("GLFW initialization failed")
+    global background_image, background_photo, canvas, background_canvas, original_image_size
+
+    # Création de la fenêtre Tkinter
+    window = tk.Tk()
+    window.title("Cat Pack Utilities V0.1")
+    window.geometry("1280x720")  # Définition de la taille de fenêtre par défaut
+
+    try:
+        # Chargement de l'image PNG pour l'arrière-plan
+        background_image = Image.open("test.png")
+        original_image_size = background_image.size  # Stockage de la taille originale de l'image
+        background_photo = ImageTk.PhotoImage(background_image)
+    except Exception as e:
+        print(f"Failed to load background image: {e}")
         return
 
-    # Create a windowed mode window and its OpenGL context
-    window = glfw.create_window(1280, 720, "Cat Pack Utilities", None, None)
-    if not window:
-        logging.error("Window or OpenGL context creation failed")
-        glfw.terminate()
-        return
+    # Création d'un canvas pour afficher l'image en arrière-plan
+    canvas = tk.Canvas(window)
+    canvas.pack(fill=tk.BOTH, expand=True)
 
-    # Set callbacks
-    glfw.set_window_size_callback(window, window_resize)
+    # Création de l'image sur le canvas
+    background_canvas = canvas.create_image(0, 0, anchor=tk.NW, image=background_photo)
 
-    # Load the PNG image as the window icon
-    icon_image = Image.open("cat.png")
-    glfw.set_window_icon(window, 1, [icon_image])
+    # Liaison de la fonction de redimensionnement à l'événement de changement de taille de fenêtre
+    window.bind("<Configure>", resize_background)
 
-    # Make the window's context current
-    glfw.make_context_current(window)
-
-    # Define the vertices of a rectangle to display the texture as an icon
-    icon_vertices = [-0.5, -0.5, 0.0,  # Bottom-left corner
-                     0.5, -0.5, 0.0,  # Bottom-right corner
-                     0.5, 0.5, 0.0,  # Top-right corner
-                     -0.5, 0.5, 0.0]  # Top-left corner
-
-    icon_vertices = (GLfloat * len(icon_vertices))(*icon_vertices)
-
-    # Vertex shader source code
-    vertex_shader_src = """
-    #version 330 core
-    layout (location = 0) in vec3 aPos;
-    void main()
-    {
-        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    }
-    """
-
-    # Fragment shader source code
-    fragment_shader_src = """
-    #version 330 core
-    out vec4 FragColor;
-    uniform sampler2D textureSampler;
-    void main()
-    {
-        FragColor = texture(textureSampler, vec2(gl_FragCoord.x / 1280.0, gl_FragCoord.y / 720.0));
-    }
-    """
-
-    # Compile shaders and link program
-    shader = shaders.compileProgram(
-        shaders.compileShader(vertex_shader_src, GL_VERTEX_SHADER),
-        shaders.compileShader(fragment_shader_src, GL_FRAGMENT_SHADER)
-    )
-    glUseProgram(shader)
-
-    # Create Vertex Array Object (VAO) and Vertex Buffer Object (VBO) for the icon rectangle
-    vao = glGenVertexArrays(1)
-    vbo = glGenBuffers(1)
-    glBindVertexArray(vao)
-    glBindBuffer(GL_ARRAY_BUFFER, vbo)
-    glBufferData(GL_ARRAY_BUFFER, len(icon_vertices) * ctypes.sizeof(GLfloat), icon_vertices, GL_STATIC_DRAW)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * ctypes.sizeof(GLfloat), ctypes.c_void_p(0))
-    glEnableVertexAttribArray(0)
-    glBindBuffer(GL_ARRAY_BUFFER, 0)
-    glBindVertexArray(0)
-
-    # Main loop
-    while not glfw.window_should_close(window):
-        glClear(GL_COLOR_BUFFER_BIT)
-
-        glUseProgram(shader)
-        glBindVertexArray(vao)
-        glActiveTexture(GL_TEXTURE0)
-
-        glUniform1i(glGetUniformLocation(shader, "textureSampler"), 0)
-        glDrawArrays(GL_QUADS, 0, 4)
-
-        glfw.swap_buffers(window)
-        glfw.poll_events()
-
-    # Terminate GLFW and cleanup
-    logging.info("Closing the application")
-    glfw.terminate()
+    # Boucle principale
+    window.mainloop()
 
 
 if __name__ == "__main__":
